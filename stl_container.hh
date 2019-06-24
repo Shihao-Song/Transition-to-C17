729efba0c369e1cdc784c20efc4cfd76c567bbd5
shihao-song <shihao.song@drexel.edu>
2019-06-23 20:16:24 -0400

    Keep working on STL container

--- stl_container.hh
+++ stl_container.hh
@@ -1,10 +1,99 @@
 #ifndef __STL_C_HH__
 #define __STL_C_HH__
 
+#include <algorithm>
 #include <vector>
+
+// Here shows O(1) complexity way to remove an item from a vector
+template<typename T>
+auto O_1_remove_at(auto &vec, T idx)
+{
+    if constexpr(std::is_same<T, std::vector<int>::iterator>::value)
+    {
+        *idx = std::move(vec.back());
+        vec.pop_back();
+    }
+    else
+    {
+        vec.at(idx) = std::move(vec.back());
+        vec.pop_back();
+    }
+}
+
 void STLContainer()
 {
+    /*
+     * Study on vector
+     * */
+    std::cout << "\n";
+    std::vector<int> v = {1, 2, 3, 4, 5, 2, 2, 8};
+    std::cout << "Size of vector: " << v.size() << "; ";
+    std::cout << "Capacity: " << v.capacity() << "\n";
+    for (auto &val : v)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
 
+    const auto new_end(remove(begin(v), end(v), 2));
+    std::cout << "Size of vector (removing 2), first: " << v.size() << "; ";
+    std::cout << "Capacity: " << v.capacity() << "\n";
+    for (auto &val : v)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    v.erase(new_end, end(v));
+    std::cout << "Size of vector (removing 2), final: " << v.size() << "; ";
+    std::cout << "Capacity: " << v.capacity() << "\n";
+    for (auto &val : v)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    auto odd([](int i) {return i % 2 != 0;});
+    v.erase(remove_if(begin(v), end(v), odd), end(v));
+    std::cout << "Size of vector (removing all odds), final: " << v.size() << "; ";
+    std::cout << "Capacity: " << v.capacity() << "\n";
+    for (auto &val : v)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    v.shrink_to_fit();
+    std::cout << "Shrink to fit (new feature), capacity: " << v.capacity() << "\n";
+
+    /*
+     * Cool way to remove item from vector
+     * */
+    std::cout << "\n";
+    std::vector<int> v2{111,222,333};
+    for (auto &val : v2)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    O_1_remove_at(v2, 1);
+    std::cout << "Removed \"222\": ";
+    for (auto &val : v2)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    O_1_remove_at(v2, std::find(begin(v2), end(v2), 333));
+    std::cout << "Removed \"333\": ";
+    for (auto &val : v2)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+    v2.shrink_to_fit();
+    std::cout << "Size: " << v2.size() << "; capacity: " << v2.capacity() << "\n";
 }
 
 #endif
