61cbabe4a00d4b6b7f58d2d3d99d8e7becb7132e
shihao-song <shihao.song@drexel.edu>
2019-06-24 09:49:52 -0400

    research on map

--- stl_container.hh
+++ stl_container.hh
@@ -2,6 +2,8 @@
 #define __STL_C_HH__
 
 #include <algorithm>
+#include <map>
+#include <unordered_map>
 #include <vector>
 
 // Here shows O(1) complexity way to remove an item from a vector
@@ -15,6 +17,7 @@ auto O_1_remove_at(auto &vec, T idx)
     }
     else
     {
+        // .at does boundary checking automatically
         vec.at(idx) = std::move(vec.back());
         vec.pop_back();
     }
@@ -94,6 +97,54 @@ void STLContainer()
     std::cout << "\n";
     v2.shrink_to_fit();
     std::cout << "Size: " << v2.size() << "; capacity: " << v2.capacity() << "\n";
+
+    /*
+     * Keep vector sorted
+     * */
+    std::cout << "\n";
+    std::vector<int> v3{1,5,7,3,2,9,10};
+    std::sort(begin(v3), end(v3));
+    auto insertSorted([](auto &v, auto ele)
+                      {
+                          auto pos = std::lower_bound(begin(v),end(v),ele);
+                          v.insert(pos, ele);
+                      });
+    insertSorted(v3, 4);
+    for (auto &val : v3)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    /*
+     * Research on std::map
+     * */
+    std::cout << "\n";
+    // New feature (1): try_emplace() - if the insertion is not successful, the object
+    // will not be constructed (copy run-time copying)
+    // New feature (2): insert with hints to speedup
+    std::map<std::string, int> m;
+    for (const auto &s : {"a", "b", "c", "a"})
+    {
+        if (auto [iter, success] = m.try_emplace(s,1);
+            !success)
+        {
+            ++(iter->second);
+        }
+    }
+    std::cout << "Mapping (character : num_occurs)\n"; 
+    for (const auto &[key,value] : m)
+    {
+        std::cout << key << " : " << value << "\n";
+    }
+
+    auto hint(end(m));
+    hint = m.insert(hint, {"d",1});
+    std::cout << "Inserted \"d\" (with hints): \n"; 
+    for (const auto &[key,value] : m)
+    {
+        std::cout << key << " : " << value << "\n";
+    }
 }
 
 #endif
